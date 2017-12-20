47a708bc4b939f330f3393c28bc8e0fd33baa1d4
Handy-Surun Dugurhap <sukhdug@gmail.com>
2017-12-21 01:10:45 +0700

    Database config class and Router class added

---
+++ config/Database.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: handy
+ * Date: 21.12.17
+ * Time: 0:08
+ */
+class Database
+{
+    public static function getConnection()
+    {
+        $paramsPath = ROOT . 'db_params.php';
+        $params = include($paramsPath);
+
+        $dsn = "mysql:host={$params['host']};dbname={$params['dbname']}";
+        $db = new PDO($dsn, $params['user'], $params['password']);
+
+        return $db;
+    }
+
+}
