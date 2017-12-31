b63ded7abd446fbd0a27f8d286538132715d46e2
Handy-Surun Dugurhap <sukhdug@gmail.com>
2017-12-31 22:56:24 +0700

    Bootstrap and Font Awesome files are connected

--- view/articles/index.php
+++ view/articles/index.php
@@ -1,4 +1,18 @@
-<?php foreach($articlesList as $articleItem){?>
-    <h1><a href="/articles/<?= $articleItem['id'] ?>"><?= $articleItem['title']; ?></a></h1>
-    <p><?= $articleItem['body']; ?></p>
-<?php } ?>
+<html>
+    <head>
+        <link href="/template/css/bootstrap.css" rel="stylesheet" type="text/css" media="screen" />
+        <link href="/template/css/font-awesome.css" rel="stylesheet" type="text/css" media="screen" />
+        <title>Главная страница</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <?php foreach($articlesList as $articleItem){?>
+                    <h1><i class="fa fa-address-book"></i>
+                        <a href="/articles/<?= $articleItem['id'] ?>"><?= $articleItem['title']; ?></a></h1>
+                    <p><?= $articleItem['body']; ?></p>
+                <?php } ?>
+            </div>
+        </div>
+    </body>
+</html>
